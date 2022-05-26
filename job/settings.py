dc6270cc0550207a83e32b25aa4af7fbb657811a
samsonnegash <samsonnegash363@gmail.com>
2022-05-26 14:19:32 +0300

    lastcommit

--- job/settings.py
+++ job/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'etdq)uvq=t0rc&ams5_ovn6w8bcwknjj0u97*(#n^(76x*+dr1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jobspace-isnp.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['jobspace-isnp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
